c22d79cb4f63839a91a0bec6de537d6d5d183b90
briac-lr <112852278+briac-lr@users.noreply.github.com>
2025-06-09 17:46:56 +0200

    Delete Abstract_Classes directory

--- Abstract_Classes/RandomVariable.cpp
+++
@@ -1,7 +0,0 @@
-#include "RandomVariable.h"
-
-RandomVariable::RandomVariable(const Distribution& distribution)
-//	: _distribution_ptr(&distribution) // DANGER : you could have a dangling pointer
-	:_distribution_ptr(distribution.clone())
-{
-}
